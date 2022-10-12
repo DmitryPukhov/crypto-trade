faea8a5bf2b285b35c7021fe4b11899c77be61a6
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2022-10-12 17:36:07 +0300

    Fixed postgres url ingestion during deployment

--- devops/deploy/deploy_jar.sh
+++ devops/deploy/deploy_jar.sh
@@ -21,10 +21,10 @@ cp -f $scala_code_dir/target/scala-2.12/$jar_name $jar_path
 pg_host_name=$(yc managed-postgresql host list --cluster-name cryptotrade-psql | awk '{print $2}' | tail -n 3 | sed ':a;N;$!ba;s/\n/ /g')
 pg_host_name=$(echo "$pg_host_name" | xargs)
 pg_host="jdbc:postgresql:\/\/$pg_host_name\:6432\/cryptotrade"
-pg_host_property="dmitrypukhov\.cryptotrade\.data\.mart\.btcusdt\.jdbc\.uri"
-echo "Set $pg_host_property=$pg_host"
+pg_host_property="dmitrypukhov\.cryptotrade\.data\.mart\.currency\.jdbc\.uri"
+echo "Set $pg_host_property=$pg_host in $app_properties_path"
 #sed "s/\($pg_host_property\s*=\s*\).*/\1${pg_host}/g" application.properties.template > application.properties
-sed "s/\($pg_host_property\s*:\s*\).*/\1${pg_host}/g" application.properties.template > $app_properties_path
+sed "s/\($pg_host_property\s*\:\s*\).*/\1${pg_host}/g" application.properties.template > $app_properties_path
 # Pack application.properties to the jar as yandex lightweight dataproc cluster doesn't support passing the file when submitting
 zip -j $jar_path $app_properties_path
 
