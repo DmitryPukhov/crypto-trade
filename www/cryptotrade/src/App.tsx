4fa0c1751a0f8077b04b5f2d301ca73cea876a7e
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2022-10-18 07:45:11 +0300

    Plotly component fixed

--- www/cryptotrade/src/App.tsx
+++ www/cryptotrade/src/App.tsx
@@ -4,50 +4,20 @@ import axios, { CancelTokenSource } from 'axios';
 import './App.css';
 import IOhlcv from './types/Ohlcv';
 import { resolveProjectReferencePath } from 'typescript';
-import PriceChart from './pricechart/PriceChart.jsx';
+import PriceChart from './pricechart/PriceChart';
 
 const defaultCandles: IOhlcv[] = [];
 
 function App() {
-  const [candles, setCandles]:[IOhlcv[], (candles: IOhlcv[])=>void] = React.useState(defaultCandles) ;
-  const [loading, setLoading]: [
-    boolean,
-    (loading: boolean) => void
-  ] = React.useState<boolean>(true);
-  const [error, setError]: [string, (error: string) => void] = React.useState(
-    ''
-  );
-  const cancelToken = axios.CancelToken; //create cancel token
-  const [cancelTokenSource, setCancelTokenSource]: [
-    CancelTokenSource,
-    (cancelTokenSource: CancelTokenSource) => void
-  ] = React.useState(cancelToken.source());
-
-  const restUrl="http://localhost:8080/candles/list";
-  React.useEffect(() => {
-
-    fetch(restUrl)
-      .then(responce=>responce.json())
-      .then((candles:IOhlcv[])=>{
-          setCandles(candles)
-      })
-  }, []);  
+  const plotly = React.createRef();
 
   return (
     <div className="App">
       <div>
-        <h1>Candles</h1>
-        <div>
-          {/* <PriceChart /> */}
+        <h1>Price analysis</h1>
+        <div id='priceChart'>
+          <PriceChart />
         </div>
-        <ul>
-          {candles.map((candle)=>(
-            <li key = {candle.datetime}>
-              {candle.datetime}, {candle.open}, {candle.high}, {candle.low}, {candle.close}
-            </li>
-          ))}
-        </ul>
-        {error && <p className="error">{error}</p>}
       </div>
     </div>
   );
