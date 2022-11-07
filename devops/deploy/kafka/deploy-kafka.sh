94ae02e19a89100e94284b1e847ce4255214c107
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2022-11-07 20:23:13 +0300

    kafka serverless container deployment created, does not work

--- devops/deploy/kafka/deploy-kafka.sh
+++ devops/deploy/kafka/deploy-kafka.sh
@@ -35,6 +35,25 @@ cp -r .ssh $tmp_dir
 
 echo "Build docker image"
 registry_id=$(yc container registry list | awk '{print $2}' | tail -n 3 | sed ':a;N;$!ba;s/\n//g')
-tag_name="cr.yandex/$registry_id/cryptotrade-kafka"
-sudo docker build -t "$tag_name" .
-sudo docker push "$tag_name"
+image_tag="cr.yandex/$registry_id/cryptotrade-kafka"
+sudo docker build -t "$image_tag" .
+#sudo docker push "$image_tag"
+
+echo "Create serverless container if does not exist"
+binance_connector_name="binance-kafka-connector"
+if [ -z "$(yc serverless container list | grep "$binance_connector_name")" ]
+then
+  yc serverless container create --name $binance_connector_name
+fi
+
+
+echo "Deploy image $image_tag to serverless container"
+service_account_id="$(yc iam service-account get cryptotrade-hadoop | head -n 1 | awk '{print $2}')"
+yc serverless container revision deploy \
+  --container-name $binance_connector_name \
+  --image $image_tag \
+  --cores 1 \
+  --memory 1GB \
+  --concurrency 1 \
+  --execution-timeout 30s \
+  --service-account-id $service_account_id
