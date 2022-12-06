e6122aaf45af9c974fb0a281e0701b79dcf08c3f
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2022-12-06 16:57:47 +0300

    start/stop scripts updated with kafka and mongodb

--- devops/deploy/start_all.sh
+++ devops/deploy/start_all.sh
@@ -1,3 +1,5 @@
+yc managed-kafka cluster start cryptotrade-kafka
 yc dataproc cluster start cryptotrade-hadoop
 yc managed-postgresql cluster start cryptotrade-psql
 yc managed-clickhouse cluster start cryptotrade-clickhouse
+yc managed-mongodb cluster start cryptotrade-mongodb
--- devops/deploy/stop_all.sh
+++ devops/deploy/stop_all.sh
@@ -1,4 +1,5 @@
+yc managed-kafka cluster stop cryptotrade-kafka
 yc dataproc cluster stop cryptotrade-hadoop
 yc managed-postgresql cluster stop cryptotrade-psql
 yc managed-clickhouse cluster stop cryptotrade-clickhouse
-yc managed-kafka cluster stop cryptotrade-kafka
+yc managed-mongodb cluster stop cryptotrade-mongodb
